3b97cbd64485a6f1e27dbc0213394d25be109396
Sana Sadiq <sanasadiq@v1040-wn-rt-a-4-102.campus-dynamic.uwaterloo.ca>
2023-03-28 15:21:41 -0400

    Added the start button

---
+++ script.js
@@ -0,0 +1,9 @@
+function startGame () {
+    // reference the HTML element you want to manipulate
+    var introSection = document.getElementById ("heading");
+
+    // change the element's CSS
+    introSection.style.display = "none";
+
+
+}
